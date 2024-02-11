d2f9650fc8ddd69cb19b92b2185fabec81233b65
Cory Redmond <cory.redmond@nandos.co.uk>
2024-02-11 01:37:54 +0000

    Tweak extractBranchName

--- src/templatefuncs/extract_ticket.ts
+++ src/templatefuncs/extract_ticket.ts
@@ -1,3 +1,5 @@
+import { refTitle } from "./strings";
+
 export const extractTicketNumber = (s: string) => {
   const ticketNumber = s.split("-").slice(0, 2).join("-").toUpperCase();
   if (ticketNumber.match(/^([A-Z]+-[0-9]+)$/)) {
@@ -7,10 +9,8 @@ export const extractTicketNumber = (s: string) => {
 };
 
 export const extractBranchName = (s: string) => {
-  const ticketNumber = s.split("-");
-  ticketNumber.shift();
-  ticketNumber.shift();
-  return ticketNumber.join(" ");
+  const ticketNumber = extractTicketNumber(s);
+  return refTitle(s.slice(0, ticketNumber.length + 1));
 };
 
 export const withPipe = (s: string) => {
